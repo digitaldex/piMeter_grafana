d84260e36c75f57a87d80b36952cd202d3b24ed3
digitaldex <andre@digitaldex.org>
2019-11-03 20:22:00 +0100

    Changes

--- main.cpp
+++ main.cpp
@@ -9,21 +9,9 @@ using namespace std;
 
 int main() {
 	spiComm commObj;
-	float WirkenergieL1, WirkenergieL2, WirkenergieL3;
-	string result;
-
-	influxdb_cpp::server_info si("server_ip", 8086, "db", "user", "pass");
-	influxdb_cpp::query(result, "select WirkenergieL1, WirkenergieL2, WirkenergieL3 from energy ORDER BY DESC LIMIT 1", si);
-
-	// Only for Startup
-	// Parse Results and Set the Init Value of Energy Variables here
+	influxdb_cpp::server_info si("IP", 8086, "DB", "user", "password");
 
 	while(1) {
-
-		WirkenergieL1 += commObj.readEnergy(R_AWATTHR_HI_REGISTER);
-		WirkenergieL2 += commObj.readEnergy(R_BWATTHR_HI_REGISTER);
-		WirkenergieL3 += commObj.readEnergy(R_CWATTHR_HI_REGISTER);
-
 		influxdb_cpp::builder()
 		    .meas("energy")
 		    .field("StromL1", commObj.readCurrent(R_AIRMS_REGISTER), 2)
@@ -41,9 +29,9 @@ int main() {
 		    .field("ScheinleistungL1", commObj.readPower(R_AVA_REGISTER), 2)
 		    .field("ScheinleistungL2", commObj.readPower(R_BVA_REGISTER), 2)
 		    .field("ScheinleistungL3", commObj.readPower(R_CVA_REGISTER), 2)
-		    .field("WirkenergieL1", WirkenergieL1, 2)
-		    .field("WirkenergieL2", WirkenergieL2, 2)
-		    .field("WirkenergieL3", WirkenergieL3, 2) 
+		    .field("WirkenergieL1", commObj.readEnergy(R_AWATTHR_HI_REGISTER), 2)
+		    .field("WirkenergieL2", commObj.readEnergy(R_BWATTHR_HI_REGISTER), 2)
+		    .field("WirkenergieL3", commObj.readEnergy(R_CWATTHR_HI_REGISTER), 2) 
 	    .post_http(si);
 
 		sleep(5);
